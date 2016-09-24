e8732b36150a8d537628d40dde4c9a76baa6b9d2
Jhonatan Leon <leon.k2@hotmail.com>
2016-09-23 23:45:37 -0500

    correccion

--- caso1/mediacontaminante.R
+++ caso1/mediacontaminante.R
@@ -1,25 +1,38 @@
 mediacontaminante<- function(directorio="~/GitHub/Programacion_Actuarial_lll_OT16/caso1/specdata",contaminante= "nitrate", id=1:332){
-  a<-numeric()
-  setwd(directorio)
-        if (id<=0){return(paste("no es valido"))
-        }
-  
+  setwd("C:/Users/Usuario/Documents/GitHub/Programacion_Actuarial_lll_OT16/caso1/specdata")
+   a<-numeric()
+   vec<-vector("numeric")
+   contador<-1
+    suma<-0
+    suma1<-0
   for(i in id){
         nuevoid<-formatC(i,width = 3, flag = "0")
         leer<-read.csv(paste(nuevoid, ".csv",sep=""))
         names(leer)
         if (contaminante=="nitrate"){
         x<-c(a,leer$nitrate)
+        xx<-na.omit(x)
+        suma<-suma+sum(x,na.rm = TRUE)
+        suma1<-suma1+length(xx)
+        
         } else if (contaminante== "sulfate"){
-          x<-c(1,leer$sulfate)
+          x<-c(a,leer$sulfate)
+          xx<-na.omit(x)
+         suma<-suma+sum(x,na.rm = TRUE)
+         suma1<-suma1+length(xx)
         }
       }
-        promedio<-mean(x,na.rm=T)
+        promedio<-suma/suma1
         promedio
         
     }
-mediacontaminante(,"nitrate",1)
-mediacontaminante(,"sulfate",3) 
+mediacontaminante("directorio","nitrate",1)
+mediacontaminante("directorio","nitrate",2)
+mediacontaminante("directorio","nitrate",1:2)
+
+mediacontaminante("directorio","sulfate",3) 
+mediacontaminante("directorio","sulfate",1:3) 
+
 
      
       
